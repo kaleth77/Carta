e5028aa8de6aadde6183bb577593951fde3aacbc
kaleth77 <kalecitopd@gmail.com>
2026-05-26 23:47:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 // script.js
 
-const fechaApertura = new Date("2026-05-30T00:00:00").getTime();
+const fechaApertura =
+new Date("2026-05-30T00:00:00").getTime();
 
-const contador = document.getElementById("contador");
+const contador =
+document.getElementById("contador");
 
-const bloqueado = document.getElementById("bloqueado");
+const bloqueado =
+document.getElementById("bloqueado");
 
-const contenido = document.getElementById("contenido");
+const contenido =
+document.getElementById("contenido");
 
 const intervalo = setInterval(() => {
 
   const ahora = new Date().getTime();
 
-  const distancia = fechaApertura - ahora;
+  const distancia =
+  fechaApertura - ahora;
 
   const dias = Math.floor(
     distancia / (1000 * 60 * 60 * 24)
   );
 
   const horas = Math.floor(
-    (distancia % (1000 * 60 * 60 * 24))
+    (distancia %
+    (1000 * 60 * 60 * 24))
     / (1000 * 60 * 60)
   );
 
   const minutos = Math.floor(
-    (distancia % (1000 * 60 * 60))
+    (distancia %
+    (1000 * 60 * 60))
     / (1000 * 60)
   );
 
   const segundos = Math.floor(
-    (distancia % (1000 * 60))
+    (distancia %
+    (1000 * 60))
     / 1000
   );
 
